0e08690b9d884e709a7b86c2e810d8ba9dcf783a
RicardoFerreira10 <42803396+RicardoFerreira10@users.noreply.github.com>
2018-09-25 10:58:40 +0100

    Update c4s-build-android.sh

--- c4s-build-android.sh
+++ c4s-build-android.sh
@@ -24,26 +24,26 @@ if [ ! -d ${SOURCE_DIR} ]; then
     tar -zxf ${SOURCE}
 fi
 
-########################################################
-################### ANDROID NDK ########################
-########################################################
-########################################################
-if [ ! -d ${ROOT_DIR}/../android-ndk/${ANDROID_NDK} ]; then
-    cd ${ROOT_DIR}/../android-ndk/
-    echo "Downloading Android NDK"
-    wget --quiet "https://dl.google.com/android/repository/${ANDROID_NDK}-linux-x86_64.zip"
-    unzip -q ${ANDROID_NDK}-linux-x86_64.zip
-    rm -rf *.zip*
-fi
+# ########################################################
+# ################### ANDROID NDK ########################
+# ########################################################
+# ########################################################
+# if [ ! -d ${ROOT_DIR}/../android-ndk/${ANDROID_NDK} ]; then
+#     cd ${ROOT_DIR}/../android-ndk/
+#     echo "Downloading Android NDK"
+#     wget --quiet "https://dl.google.com/android/repository/${ANDROID_NDK}-linux-x86_64.zip"
+#     unzip -q ${ANDROID_NDK}-linux-x86_64.zip
+#     rm -rf *.zip*
+# fi
 
-cd ${ROOT_DIR}
+# cd ${ROOT_DIR}
 
-echo "Setting Android NDK environment"
+# echo "Setting Android NDK environment"
 
-set -e
-NDK_ROOT=$(readlink -f "../android-ndk/${ANDROID_NDK}")
+# set -e
+# NDK_ROOT=$(readlink -f "../android-ndk/${ANDROID_NDK}")
 
-exit 0
+# exit 0
 # export ANDROID_NDK_ROOT=${NDK_ROOT}
 # export ANDROID_NDK_HOME=${NDK_ROOT}
 # export NDK_PLATFORM=android-21
